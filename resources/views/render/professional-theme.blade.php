1c956a9acf9352f5a049a9dece0d2437c8a66f52
Dylan Baine <dylan_baine@sweetwater.com>
2023-10-26 22:03:08 -0400

    more professional theme

--- resources/views/render/professional-theme.blade.php
+++ resources/views/render/professional-theme.blade.php
@@ -3,26 +3,33 @@
     height: 100%;
     position: relative;
     display: flex;
+    padding: 20px;
     ">
         <div style="width: 50%; display: flex; align-items: center;">
-            <img style="width: 100%;" src="/api/v2/image.png?url={{ $url }}" alt="" srcset="">
+            <img style="
+            width: 100%;
+            border-radius: 12px;
+            box-shadow: 1px 2px 20px black;
+            "
+                src="/api/v2/image.png?url={{ $url }}" alt="" srcset="">
         </div>
         <div style="width: 50%; display: flex; align-items: center; justify-content: center;">
             <div style="text-align: center;">
-                <h1 style="font-weight: bold; font-size: 3em;">{{ $domain }}</h1>
+                <h1 style="font-weight: bold; font-size: 3em;">READ MORE</h1>
                 <button
                     style="
+                    margin-top: -42px;
                     background: black;
                     bottom: 40px;
                     color: white;
                     font-size: 2em;
-                    padding: .8em 1.6em;
+                    padding: .5em 1em;
                     font-weight: bold;
                     outline: none;
                     border-radius: .5em;
                     border: none;
                     ">
-                    READ MORE
+                    {{ $domain }}
                 </button>
             </div>
         </div>
