7203e297ef91527154c5987529344903e9ab10de
Dylan Baine <dylan_baine@sweetwater.com>
2023-11-24 10:47:16 -0500

    body bg color

--- resources/views/components/render-wrapper.blade.php
+++ resources/views/components/render-wrapper.blade.php
@@ -4,7 +4,7 @@
 <head>
 </head>
 
-<body style="padding: 0; margin: 0;">
+<body style="padding: 0; margin: 0; background-color: white;">
     <div style="width: 100vw; display: flex; justify-content: center; align-items: center; color: rgb(56, 55, 51);">
         <div
             style="width: 1200px; height: 630px; margin: auto; display: flex; justify-content: center; align-items: center; font-size: 1.3em; border: solid 1px rgb(151, 151, 151);">
